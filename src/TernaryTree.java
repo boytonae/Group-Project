3e2a5a59686717c594f4b119f9ef785cef171d24
hellomynameistj <Thomas.slate93@gmail.com>
2014-12-17 12:45:33 +0000

    stuff

---
+++ src/TernaryTree.java
@@ -0,0 +1,18 @@
+
+public class TernaryTree<T> 
+{
+	private T element;
+	private TernaryTree<T> left;
+	private TernaryTree<T> centre;
+	private TernaryTree<T> right;
+
+	public TernaryTree(T element)
+	{
+		this.element = element;
+	}
+	
+public void addLeft
+
+
+
+}
